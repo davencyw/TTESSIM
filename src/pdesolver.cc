cf061cab7e69130cad1e247f3fc1ad663c26970c
davschmi <davschmi@student.ethz.ch>
2017-10-16 16:21:05 +0200

    added icc support

--- src/pdesolver.cc
+++ src/pdesolver.cc
@@ -10,7 +10,13 @@
 bool Pdesolver::solvefluid(precision_t* __restrict__ fluid_temperature, precision_t* __restrict__ fluid_temperature_o){
 	
 	//Loop over inner N-2 cells
+	
+	#ifdef __INTEL_COMPILER
+	#pragma ivdep
+	#elif __GNUC__
 	#pragma GCC ivdep
+	#endif
+	
 	for (int i = 1; i < _simenv->_numcells-1; ++i)
 	{		const precision_t tfi = fluid_temperature[i];
 			const precision_t tfim1 = fluid_temperature[i-1];
@@ -30,7 +36,13 @@ bool Pdesolver::solvefluid(precision_t* __restrict__ fluid_temperature, precisio
 bool Pdesolver::solvesolid(precision_t* __restrict__ solid_temperature, precision_t* __restrict__ solid_temperature_o){
 
 	//Loop over inner N-2 cells
+	
+	#ifdef __INTEL_COMPILER
+	#pragma ivdep
+	#elif __GNUC__
 	#pragma GCC ivdep
+	#endif
+
 	for (int i = 0; i < _simenv->_numcells-1; ++i)
 	{
 		const precision_t tsi = solid_temperature[i];
