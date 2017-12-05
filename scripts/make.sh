beb068a0b9a6e2706201dddc93ff3c3389d69f93
davschmi <davschmi@student.ethz.ch>
2017-12-05 14:21:49 +0100

    changed makefile to generate build folder and makefile

--- scripts/make.sh
+++ scripts/make.sh
@@ -8,7 +8,13 @@ echo "______\n"
 echo "make.sh"
 echo "______\n"
 
-#make
+#build does not exist yet
+mkdir -p ../build
 cd ../build
+#create makefile if it does not exist
+if [ ! -f Makefile ]; then
+	cmake ..
+fi
+#make
 make
 cd -
